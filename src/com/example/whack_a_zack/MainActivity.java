9ffacc347d1fdd13666d3a2fe39139af0f9c0e17
lag47 <lag4777@gmail.com>
2014-06-03 17:25:20 -0400

    added code to onCreate method

--- src/com/example/whack_a_zack/MainActivity.java
+++ src/com/example/whack_a_zack/MainActivity.java
@@ -1,23 +1,41 @@
 package com.example.whack_a_zack;
 
+import com.example.whack_a_zack.R;
+
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.os.Build;
 
 public class MainActivity extends Activity {
+	private static final int SELECT_PICTURE = 1;
+	private String selectedImagePath;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
+		setContentView(R.layout.activity_main); // make sure you actually
+												// create button
+		(findViewById(R.id.button1)).setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				// in onCreate or any event where you want user to select file
+				Intent intent = new Intent();
+				intent.setType("image/*");
+				intent.setAction(Intent.ACTION_GET_CONTENT);
+				startActivityForResult(
+						Intent.createChooser(intent, "Select Picture"),
+						SELECT_PICTURE);
+			}
+			// add button later
+		});
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
