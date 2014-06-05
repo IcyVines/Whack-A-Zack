862e26e89c6ef60174f6aa7d6ab7262b6e5d60d6
lag47 <lag4777@gmail.com>
2014-06-05 08:39:37 -0400

    got rid of any imager select stuff and initialized buttons

--- src/com/example/whack_a_zack/MainActivity.java
+++ src/com/example/whack_a_zack/MainActivity.java
@@ -17,84 +17,42 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.os.Build;
 import android.provider.MediaStore;
 
 public class MainActivity extends Activity {
 	private static final int SELECT_PICTURE = 1;
-	private String selectedImagePath;
-	private String filemanagerstring;
-	private ImageView selectedImage;
+	//private String selectedImagePath;
+	//private String filemanagerstring;
+	private ImageButton hole1,hole2,hole3,hole4,hole5,hole6,hole7,hole8,hole9;
+	private ImageButton[] holes= new ImageButton[9];
+	private EditText timeText,scoreText;
+	//private ImageView selectedImage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		selectedImage = (ImageView) (findViewById(R.id.img));
-		(findViewById(R.id.button1)).setOnClickListener(new OnClickListener() {
-			public void onClick(View arg0) {
-				// in onCreate or any event where you want user to select file
-				Intent intent = new Intent();
-				intent.setType("image/*");
-				intent.setAction(Intent.ACTION_GET_CONTENT);
-				startActivityForResult(
-						Intent.createChooser(intent, "Select Picture"),
-						SELECT_PICTURE);
-			}
-			// add button later
-		});
+	/*	hole1= (ImageButton)(findViewById(R.id.hole1));
+		hole2= (ImageButton)(findViewById(R.id.hole2));
+		hole3= (ImageButton)(findViewById(R.id.hole3));
+		hole4= (ImageButton)(findViewById(R.id.hole4));
+		hole5= (ImageButton)(findViewById(R.id.hole5));
+		hole6= (ImageButton)(findViewById(R.id.hole6));
+		hole7= (ImageButton)(findViewById(R.id.hole7));
+		hole8= (ImageButton)(findViewById(R.id.hole8));
+		hole9= (ImageButton)(findViewById(R.id.hole9));*/
+	
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
 
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == RESULT_OK) {
-			if (requestCode == SELECT_PICTURE) {
-				Uri selectedImageUri = data.getData();
-
-				// OI FILE Manager
-				filemanagerstring = selectedImageUri.getPath();
-
-				// MEDIA GALLERY
-				selectedImagePath = getPath(selectedImageUri);
-
-				// DEBUG PURPOSE - you can delete this if you want
-				if (selectedImagePath != null)
-					System.out.println(selectedImagePath);
-				else
-					System.out.println("selectedImagePath is null");
-				if (filemanagerstring != null)
-					System.out.println(filemanagerstring);
-				else
-					System.out.println("filemanagerstring is null");
-
-				// NOW WE HAVE OUR WANTED STRING
-				if (selectedImagePath != null)
-					System.out
-							.println("selectedImagePath is the right one for you!");
-				else
-					System.out
-							.println("filemanagerstring is the right one for you!");
-			}
-		}
-	}
-
-	public String getPath(Uri uri) {
-		String[] projection = { MediaStore.Images.Media.DATA };
-		Cursor cursor = managedQuery(uri, projection, null, null, null);
-		if (cursor != null) {
-			// HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
-			// THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
-			int column_index = cursor
-					.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-			cursor.moveToFirst();
-			return cursor.getString(column_index);
-		} else
-			return null;
-	}
+	
 
 
 	@Override
