882a60fce090a3aeb03fbcf0fc6e3ea5bced4218
Roy Keren <icyvines@gmail.com>
2014-06-12 20:18:12 -0400

    Working besides Scoreboard and Sound

--- gen/com/example/whack_a_zack/R.java
+++ gen/com/example/whack_a_zack/R.java
@@ -22,57 +22,34 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int whackzack=0x7f020001;
+        public static final int backdrop=0x7f020000;
+        public static final int background=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int whackzack=0x7f020003;
     }
     public static final class id {
-<<<<<<< HEAD
-        public static final int action_settings=0x7f080014;
-        public static final int hole1=0x7f080007;
-        public static final int hole2=0x7f080008;
-        public static final int hole3=0x7f080009;
-        public static final int hole4=0x7f08000b;
-        public static final int hole5=0x7f08000c;
-        public static final int hole6=0x7f08000d;
-        public static final int hole7=0x7f08000f;
-        public static final int hole8=0x7f080010;
-        public static final int hole9=0x7f080011;
-        public static final int life=0x7f080004;
-        public static final int life_count=0x7f080005;
-        public static final int score=0x7f080013;
-        public static final int start=0x7f080003;
-        public static final int tableRow0=0x7f080000;
-        public static final int tableRow1=0x7f080006;
-        public static final int tableRow2=0x7f08000a;
-        public static final int tableRow3=0x7f08000e;
-        public static final int tableRow4=0x7f080012;
-        public static final int time=0x7f080001;
-        public static final int time_count=0x7f080002;
-=======
-        public static final int EditText01=0x7f080007;
-        public static final int TextView01=0x7f080006;
-        public static final int action_settings=0x7f080016;
-        public static final int hole1=0x7f080009;
-        public static final int hole2=0x7f08000a;
-        public static final int hole3=0x7f08000b;
-        public static final int hole4=0x7f08000d;
-        public static final int hole5=0x7f08000e;
-        public static final int hole6=0x7f08000f;
-        public static final int hole7=0x7f080011;
-        public static final int hole8=0x7f080012;
-        public static final int hole9=0x7f080013;
+        public static final int EditText01=0x7f080006;
+        public static final int TextView01=0x7f080005;
+        public static final int action_settings=0x7f080015;
+        public static final int hole1=0x7f080008;
+        public static final int hole2=0x7f080009;
+        public static final int hole3=0x7f08000a;
+        public static final int hole4=0x7f08000c;
+        public static final int hole5=0x7f08000d;
+        public static final int hole6=0x7f08000e;
+        public static final int hole7=0x7f080010;
+        public static final int hole8=0x7f080011;
+        public static final int hole9=0x7f080012;
         public static final int life=0x7f080001;
         public static final int life_count=0x7f080002;
-        public static final int start=0x7f080014;
+        public static final int start=0x7f080013;
         public static final int tableRow0=0x7f080000;
-        public static final int tableRow1=0x7f080008;
-        public static final int tableRow2=0x7f08000c;
-        public static final int tableRow3=0x7f080010;
-        public static final int tableRow4=0x7f080015;
-        public static final int tableRow5=0x7f080005;
+        public static final int tableRow1=0x7f080007;
+        public static final int tableRow2=0x7f08000b;
+        public static final int tableRow3=0x7f08000f;
+        public static final int tableRow4=0x7f080014;
         public static final int time=0x7f080003;
         public static final int time_count=0x7f080004;
->>>>>>> e3f68022f777581da57311725746d1dee658a5af
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/whack_a_zack/MainActivity.java
+++ src/com/example/whack_a_zack/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.whack_a_zack;
 import com.example.whack_a_zack.R;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,54 +12,36 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-<<<<<<< HEAD
-import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+
+//import android.widget.ImageView;
 import android.widget.TextView;
-import android.os.Build;
-import android.provider.MediaStore;
-=======
-import android.widget.TextView;
+//import android.os.Build;
+//import android.provider.MediaStore;
+//import android.widget.TextView;
 
-<<<<<<< HEAD
-
->>>>>>> e3f68022f777581da57311725746d1dee658a5af
-
-public class MainActivity extends Activity implements OnClickListener{
-	//private static final int SELECT_PICTURE = 1;
-	//private String selectedImagePath;
-	//private String filemanagerstring;
-	private int score=0;
-	private int time;
-	//private int lives=3;
-	private ImageButton hole1,hole2,hole3,hole4,hole5,hole6,hole7,hole8,hole9;
-	private ImageButton[] holes= new ImageButton[9];
-<<<<<<< HEAD
-	private Button start;
-	private TextView timeText,scoreText;
-=======
-	private TextView timeText,scoreText;
-	private Button startButton;
->>>>>>> e3f68022f777581da57311725746d1dee658a5af
-	//private ImageView selectedImage;
-	
-=======
 public class MainActivity extends Activity implements OnClickListener {
 	// private static final int SELECT_PICTURE = 1;
 	// private String selectedImagePath;
 	// private String filemanagerstring;
 	private int score = 0;
-	private int time;
-	// private int lives=3;
-	private ImageButton hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8,
-			hole9;
+	private int time = 30;
+	private int wait = 500, disappear = 5000, limit = 0;
+	private Boolean clicked = false;
+	private int lives = 2;
+	private ImageButton hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9;
 	private ImageButton[] holes = new ImageButton[9];
-	private TextView timeText, scoreText;
+	private TextView timeText, scoreText, lifeText;
 	private Button startButton;
-	private Boolean started = false;
-
+	Timer timer, newEnemy;
+	private int enemyHole = 0;
 	// private ImageView selectedImage;
 
->>>>>>> 1c70b6b4f599822601d28e28b7fe1b96d7ee473e
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -70,108 +53,28 @@ public class MainActivity extends Activity implements OnClickListener {
 			@Override
 			public void onClick(View v) {
 				startGame();
-				started=true;
 			}
 
 		});
-<<<<<<< HEAD
-		scoreText= (EditText)(findViewById(R.id.EditText01));
-		hole1= (ImageButton)(findViewById(R.id.hole1));
-<<<<<<< HEAD
-		holes[0]=hole1;
-		hole2= (ImageButton)(findViewById(R.id.hole2));
-		holes[1]=hole2;
-		hole3= (ImageButton)(findViewById(R.id.hole3));
-		holes[2]=hole3;
-		hole4= (ImageButton)(findViewById(R.id.hole4));
-		holes[3]=hole4;
-		hole5= (ImageButton)(findViewById(R.id.hole5));
-		holes[4]=hole5;
-		hole6= (ImageButton)(findViewById(R.id.hole6));
-		holes[5]=hole6;
-		hole7= (ImageButton)(findViewById(R.id.hole7));
-		holes[6]=hole7;
-		hole8= (ImageButton)(findViewById(R.id.hole8));
-		holes[7]=hole8;
-		hole9= (ImageButton)(findViewById(R.id.hole9));
-=======
-		//hole1.setOnClickListener(this);
-		holes[0]=hole1;
-		hole2= (ImageButton)(findViewById(R.id.hole2));
-		//hole2.setOnClickListener(this);
-		holes[1]=hole2;
-		hole3= (ImageButton)(findViewById(R.id.hole3));
-		//hole3.setOnClickListener(this);
-		holes[2]=hole3;
-		hole4= (ImageButton)(findViewById(R.id.hole4));
-		//hole4.setOnClickListener(this);
-		holes[3]=hole4;
-		hole5= (ImageButton)(findViewById(R.id.hole5));
-		//hole5.setOnClickListener(this);
-		holes[4]=hole5;
-		hole6= (ImageButton)(findViewById(R.id.hole6));
-		//hole6.setOnClickListener(this);
-		holes[5]=hole6;
-		hole7= (ImageButton)(findViewById(R.id.hole7));
-		//hole7.setOnClickListener(this);
-		holes[6]=hole7;
-		hole8= (ImageButton)(findViewById(R.id.hole8));
-		//hole8.setOnClickListener(this);
-		holes[7]=hole8;
-		hole9= (ImageButton)(findViewById(R.id.hole9));
-		//hole9.setOnClickListener(this);
->>>>>>> e3f68022f777581da57311725746d1dee658a5af
-		holes[8]=hole9;
-		for(ImageButton hole : holes){
-			hole.setVisibility(2);
-			hole.setClickable(false);
-			hole.setOnClickListener(this);
-		}
+
+		scoreText = (EditText)(findViewById(R.id.EditText01));
 		timeText = (EditText)(findViewById(R.id.time_count));
-		timeText.setText("" + 60);
-		start = (Button)(findViewById(R.id.start));
-		start.setOnClickListener(this);
-	}
+		lifeText = (EditText)(findViewById(R.id.life_count));
+		
+		holes[0] = hole1 = (ImageButton)(findViewById(R.id.hole1));
+		holes[1] = hole2 = (ImageButton)(findViewById(R.id.hole2));
+		holes[2] = hole3 = (ImageButton)(findViewById(R.id.hole3));
+		holes[3] = hole4 = (ImageButton)(findViewById(R.id.hole4));
+		holes[4] = hole5 = (ImageButton)(findViewById(R.id.hole5));
+		holes[5] = hole6 = (ImageButton)(findViewById(R.id.hole6));
+		holes[6] = hole7 = (ImageButton)(findViewById(R.id.hole7));
+		holes[7] = hole8 = (ImageButton)(findViewById(R.id.hole8));
+		holes[8] = hole9 = (ImageButton)(findViewById(R.id.hole9));
 
-	@Override
-	public void onClick(View v) {
-		if(v.getId() == R.id.start){
-			countDown();
+		for (int i = 0; i <= 8; i++) {
+			holes[i].setImageDrawable(null);
+			holes[i].setOnClickListener(null);
 		}
-	}
-	
-	private void countDown(){
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		timeText.setText(Integer.parseInt(timeText.getText().toString()) - 1 + "");
-		if(!timeText.getText().toString().equals(0))
-			countDown();
-=======
-		scoreText = (EditText) (findViewById(R.id.EditText01));
-		hole1 = (ImageButton) (findViewById(R.id.hole1));
-		holes[0] = hole1;
-		hole2 = (ImageButton) (findViewById(R.id.hole2));
-		holes[1] = hole2;
-		hole3 = (ImageButton) (findViewById(R.id.hole3));
-		holes[2] = hole3;
-		hole4 = (ImageButton) (findViewById(R.id.hole4));
-		holes[3] = hole4;
-		hole5 = (ImageButton) (findViewById(R.id.hole5));
-		holes[4] = hole5;
-		hole6 = (ImageButton) (findViewById(R.id.hole6));
-		holes[5] = hole6;
-		hole7 = (ImageButton) (findViewById(R.id.hole7));
-		holes[6] = hole7;
-		hole8 = (ImageButton) (findViewById(R.id.hole8));
-		holes[7] = hole8;
-		hole9 = (ImageButton) (findViewById(R.id.hole9));
-		holes[8] = hole9;
-
->>>>>>> 1c70b6b4f599822601d28e28b7fe1b96d7ee473e
 	}
 
 	@Override
@@ -198,19 +101,6 @@ public class MainActivity extends Activity implements OnClickListener {
 	 * A placeholder fragment containing a simple view.
 	 */
 
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 1c70b6b4f599822601d28e28b7fe1b96d7ee473e
-	@Override
-	public void onClick(View v) {
-		score += 10;
-		scoreText.setText(score + "");
-
-	}
-
 	/*
 	 * public void setToFriendly(ImageButton b){
 	 * //b.setImageDrawable(R.drawable.whackzack); b.setOnClickListener(new
@@ -223,34 +113,164 @@ public class MainActivity extends Activity implements OnClickListener {
 	 * 
 	 * }); }
 	 */
+	
+	@Override
+	public void onClick(View v) {
+		this.clicked = true;
+		score += 10;
+		scoreText.setText(score + "");
+		removeEnemy();		
+	}
+	
+	public void removeEnemy(){
+		holes[enemyHole].setOnClickListener(null);
+		holes[enemyHole].setImageDrawable(null);
+	}
+	
+	public void waitFor(){
+		newEnemy.cancel();
+		newEnemy.purge();
+		newEnemy = new Timer();
+		newEnemy.schedule(new TimerTask() {
+		 @Override
+		 public void run(){
+			 MainActivity.this.runOnUiThread(new Runnable(){
+				 public void run(){
+					 wait -= 10;
+					 getEnemy();
+				 }
+			 });
+		 }
+	 }, wait);
+	}
+	
+	public void loseLife(){
+		lifeText.setText(lives-- + "");
+		if(lives < 0){
+			newEnemy.cancel();
+			newEnemy.purge();
+			restartGame();
+		}
+	}
+	
+	public void getEnemy(){
+		int enemyHole = (int) (Math.random() * 9);
+		this.enemyHole = enemyHole;
+		setToEnemy(holes[enemyHole]);
+	}
+	
 	public void setToEnemy(ImageButton b) {
+		b.setImageResource(R.drawable.whackzack);
 		b.setOnClickListener(this);
+		newEnemy.cancel();
+		newEnemy.purge();
+		newEnemy = new Timer();
+		newEnemy.schedule(new TimerTask() {
+			@Override
+			public void run(){
+				MainActivity.this.runOnUiThread(new Runnable(){ 
+					@Override
+					public void run(){
+						if(MainActivity.this.clicked){
+							MainActivity.this.clicked = false;
+							limit = 0;
+							disappear *= .95;
+							waitFor();
+						} else {
+							if(limit >= disappear){
+								if(lives == 0){
+									limit = 0;
+									disappear = 5000;
+									loseLife();
+								} else {
+									loseLife();
+									disappear *= .90;
+									limit = 0;
+									removeEnemy();
+									waitFor();
+								}
+							} else {
+								limit += 100;
+							}
+						}	
+					}
+				});
+			}
+		}, 0, 100);
+	}
+	
+	public void startGame(){
+		//startButton.setVisibility(startButton.INVISIBLE);
+		startButton.setText("Stop Playing");
+		startButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				restartGame();
+			}
+
+		});
+		timer = new Timer();
+		timer.schedule(new UpdateTimer(), 1000, 1000);
+		newEnemy = new Timer();
+		waitFor();
+	}
+	
+	class UpdateTimer extends TimerTask{
+		
+		@Override
+		public void run(){
+			MainActivity.this.runOnUiThread(new Runnable() {
+				@Override
+				public void run(){
+					timeText.setText(--time + "");
+					if(time == 0){
+						scoreBoard();
+						restartGame();
+					}
+				}
+			});	
+		}
 	}
 
-	public void startGame() {
+	public void scoreBoard(){
+		
+	}
+	
+	public void restartGame() {
+		timer.cancel();
+		timer.purge();
+		newEnemy.cancel();
+		newEnemy.purge();
+		//startButton.setVisibility(startButton.VISIBLE);
+		startButton.setText("Start Whacking");
+		startButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startGame();
+			}
+
+		});
+		timer.cancel();
+		timer.purge();
+		newEnemy.cancel();
+		newEnemy.purge();
 		for (int i = 0; i <= 8; i++) {
 			holes[i].setImageDrawable(null);
 			holes[i].setOnClickListener(null);
 		}
-		long endTime = System.currentTimeMillis() + 1000;
-		long updateTime;
-		while (System.currentTimeMillis() < endTime) {
-			timeText.setText((int) ((endTime - System.currentTimeMillis()) / 1000)
-					+ "");
-			int enemyHole = (int) (Math.random() * 9);
-			setToEnemy(holes[enemyHole]);
-			updateTime = System.currentTimeMillis() + 1000;
-			while (System.currentTimeMillis() < updateTime)
-				;
-		}
+		timeText.setText(30 + "");
+		time = 30;
+		score = 0;
+		lifeText.setText(3 + "");
+		lives = 2;
+		scoreText.setText(0 + "");
 	}
-<<<<<<< HEAD
->>>>>>> e3f68022f777581da57311725746d1dee658a5af
-=======
 
-	public void playGame() {
-		
-
+	public void onDestroy(){
+		super.onDestroy();
+		timer.cancel();
+		newEnemy.cancel();
 	}
->>>>>>> 1c70b6b4f599822601d28e28b7fe1b96d7ee473e
 }
